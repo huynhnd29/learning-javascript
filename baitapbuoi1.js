de46b0a8d72fe10ad2733d312c3e294bc266962e
huynhnd1 <huynhnd@techlead.vn>
2023-11-13 14:43:23 +0700

    Bai tap buoi 1

---
+++ baitapbuoi1.js
@@ -0,0 +1,60 @@
+// Viết chương trình JavaScript để kiểm tra xem một số nguyên là dương, âm hay bằng 0 và in ra kết quả.
+// const checkNumber = 0;
+// if (checkNumber > 0) {
+//     console.log("Biến dương")
+// } else if (checkNumber < 0) {
+//     console.log("Biến âm")
+// } else {
+//     console.log("Biến bằng 0")
+// }
+
+// Viết chương trình JavaScript để kiểm tra xem một chuỗi có độ dài lớn hơn 5 ký tự không. Nếu có, in ra "Chuỗi quá dài"; ngược lại, in ra "Chuỗi đủ ngắn".
+// const chuoiKyTu = "Javascript"
+// const doDaiChuoi = chuoiKyTu.length;
+// if (doDaiChuoi > 5) {
+//     console.log("Độ dài chuỗi bằng: ", doDaiChuoi, ", Chuỗi quá dài")
+// } else {
+//     console.log("Độ dài chuỗi bằng: ", doDaiChuoi, ", Chuỗi đủ ngắn")
+// }
+
+// Check năm nhuận
+// const year = 2004;
+
+// if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//     console.log("Năm",year, "là năm nhuận")
+// }else{
+//     console.log("Năm",year, "Không phải năm nhuận")
+// }
+
+// Tạo một biến day và kiểm tra xem day có phải là một ngày hợp lệ trong tháng (ví dụ: ngày 1-31) không.
+// const day = 1; 
+// if(day <= 31 && day >= 1){
+//     console.log("Ngày", day, "Là ngày hợp lệ")
+// }else {
+//     console.log("Ngày", day, "Là ngày không hợp lệ")
+// }
+
+// Viết chương trình JavaScript để tìm số lớn nhất trong ba số nguyên a, b và c.
+// const a = 2
+// const b = 4
+// const c = 8
+// if (a > b && a > c) {
+//     console.log("a lớn nhât")
+// } else if (b > a && b > c) {
+//     console.log("b lớn nhât")
+// } else {
+//     console.log("c lớn nhât")
+// }
+
+// Viết chương trình JavaScript để kiểm tra xem một số nguyên có phải là số Palindrome (số đối xứng) hay không - số có 3 chữ số.
+
+// const number = 939 ;
+// const tailNumber = number % 10
+// const headNumber = (number - number % 100) / 100
+
+// if(tailNumber === headNumber){
+//     console.log("Số nguyên đối xứng")
+// }else{
+//     console.log("Số nguyên không đối xứng")
+// }
+
---
+++ buoi1.js
@@ -0,0 +1,67 @@
+// Khai báo biến let, const, var
+// Kiểu dữ liệu 
+// Number : số
+// String: Text, chuỗi ký tự
+// Boolean: true / false
+// null
+// undefined
+// Object
+
+// const bienNumber = 5;
+// const bienString = "Learning javascript"
+// const bienBoolean = true
+// const bienNull = null
+// const bienUndefined = undefined
+// const bienObject = {
+//     firstName: "Nguyễn",
+//     lastName: "Huynh",
+//     age: 24,
+//     job: "IT",
+//     isTeacher: true,
+//     skill: {
+//         tenis: false,
+//         eSport: true,
+//     }
+// }
+// console.log(bienNumber)
+// console.log(bienString)
+// console.log(bienNull)
+// console.log(bienUndefined)
+// console.log(bienObject.skill.tenis)
+
+//toán tử phép tính
+// + - * /
+// ++ , --, %, 
+// +=, -=, *=, /=, %=
+// let a = 5
+// a /= 2 // a = a + 2
+// a /= 2 // a = a + 2
+// a /= 5 // a = a + 2
+
+// console.log(a)
+
+// toán tử so sánh 
+// ==, ===, !=, !==, >, < >=, <=
+// const a = "20";
+// const b = 20;
+
+// console.log(a === b) // false
+
+// Câu lệnh rẽ nhánh
+
+// if
+// else if
+// else
+
+// const a = 321;
+// const b = 321
+
+// if (a > b) {
+//     console.log("A lớn hơn B")
+// } else if (a < b) {
+//     console.log("A nhỏ hơn B")
+// } else {
+//     console.log("A Bằng B")
+// }
+
+
